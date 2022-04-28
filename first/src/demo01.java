f78ded1e69dddba061cb325e3c3cab42a0c557f9
Yaojl <903337655@qq.com>
2022-04-28 13:20:32 +0800

    B功能

--- first/src/demo01.java
+++ first/src/demo01.java
@@ -2,5 +2,6 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("第一行代码");
         System.out.println("A功能");
+        System.out.println("B功能");
     }
 }
